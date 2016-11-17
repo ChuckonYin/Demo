3c56c34a74e3f850dc0d0d10b027dd798768df76
yinxukun <YINXUKUN137@pingan.com.cn>
2016-11-17 14:23:34 +0800

    no message

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by yinxukun on 2016/11/17.
+//  Copyright © 2016年 pinAn.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
